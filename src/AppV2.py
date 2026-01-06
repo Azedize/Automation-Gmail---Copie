1a9b20e1d1d2a449ba5b31e0f156ee69f893e082
azedine chentouf <azedinechentouf0@gmail.com>
2026-01-06 14:01:13 +0100

    ccccccccccccccdddddddddd

--- src/AppV2.py
+++ src/AppV2.py
@@ -280,71 +280,6 @@ def Generate_User_Input_Data(window):
 
 
 
-# 🛠️ Démarre le processus d'extraction en lançant le thread principal avec les paramètres utilisateur, après validation des entrées et préparation de l'environnement.
-def Start_Extraction(window, data_list, entered_number , selected_Browser , Isp , unique_id , output_json_final , username):
-    global EXTRACTION_THREAD 
-    DevLogger.info("Starting extraction process...")
-    
-    ValidationUtils.ensure_path_exists(Settings.LOGS_DIRECTORY)
-    
-    try:
-        entered_number = int(entered_number)
-    except ValueError:
-        UIManager.Show_Critical_Message(
-            window,
-            "Input Error - Invalid Format",
-            "Numeric value required. Please check your input and try again.",
-            message_type="critical"
-        )
-
-
-        return
-
-    email_count = len(data_list)
-    if entered_number > email_count:
-        UIManager.Show_Critical_Message(
-            window,
-            "Range Error - Exceeded Limit",
-            f"Maximum allowed entries: {email_count}\n"
-            f"Please enter a value between 1 and {email_count}.",
-            message_type="critical"
-        )
-        return
-    DevLogger.info("Selected entries:", entered_number)
-
-
-
-
-    Launch_Close_Chrome(selected_Browser , username)
-    browser_path = (
-        BrowserManager.get_browser_path("chrome.exe") if selected_Browser.lower() == "chrome"
-        else BrowserManager.get_browser_path("firefox") if selected_Browser.lower() == "firefox"
-        else BrowserManager.get_browser_path("msedge.exe") if selected_Browser.lower() == "edge"
-        else BrowserManager.get_browser_path("dragon.exe")  
-    )
-  
-
-    if selected_Browser.lower() == "firefox":
-        ensure_web_ext_installed()
-
-    DevLogger.info("browser path   :",   browser_path    or "Non trouvé")
-
-    # return browser_path;
-    EXTRACTION_THREAD = ExtractionThread(
-        data_list, SESSION_ID, entered_number, browser_path , window ,selected_Browser , Isp , unique_id , output_json_final
-    )
-    # data_list, SESSION_ID, entered_number, Browser_path, main_window ,selected_Browser,Isp , unique_id , output_json_final
-    EXTRACTION_THREAD.progress.connect(lambda msg: print(msg))
-    EXTRACTION_THREAD.finished.connect(lambda: QMessageBox.information(window, "Terminé", "L'extraction est terminée."))
-    EXTRACTION_THREAD.stopped.connect(lambda msg: QMessageBox.warning(window, "Arrêté", msg))
-    EXTRACTION_THREAD.start()
-
-
-
-
-
-
-
 
 def Save_Email(params):
     return str(APIManager.save_email(params))
@@ -1532,9 +1467,113 @@ class MainWindow(QMainWindow):
 
 
         with ThreadPoolExecutor(max_workers=2) as executor:
-            executor.submit(Start_Extraction, window, data_list , entered_number, selected_Browser, self.Isp.currentText() , unique_id , result_json, session_info["username"])
+            executor.submit(self.Start_Extraction, window, data_list , entered_number, selected_Browser, self.Isp.currentText() , unique_id , result_json, session_info["username"])
             executor.submit(self.LOGS_THREAD.start)
+        
+
+
+
+
+
+
+
+
+
+
+
+
+    # 🛠️ Démarre le processus d'extraction en lançant le thread principal avec les paramètres utilisateur, après validation des entrées et préparation de l'environnement.
+    def Start_Extraction(self, window, data_list, entered_number , selected_Browser , Isp , unique_id , output_json_final , username):
+        global EXTRACTION_THREAD 
+        DevLogger.info("Starting extraction process...")
+        
+        ValidationUtils.ensure_path_exists(Settings.LOGS_DIRECTORY)
+        
+        try:
+            entered_number = int(entered_number)
+        except ValueError:
+            UIManager.Show_Critical_Message(
+                window,
+                "Input Error - Invalid Format",
+                "Numeric value required. Please check your input and try again.",
+                message_type="critical"
+            )
+
+
+            return
+
+        email_count = len(data_list)
+        if entered_number > email_count:
+            UIManager.Show_Critical_Message(
+                window,
+                "Range Error - Exceeded Limit",
+                f"Maximum allowed entries: {email_count}\n"
+                f"Please enter a value between 1 and {email_count}.",
+                message_type="critical"
+            )
+            return
+        DevLogger.info("Selected entries:", entered_number)
+
+
+
+
+        Launch_Close_Chrome(selected_Browser , username)
+        browser_path = (
+            BrowserManager.get_browser_path("chrome.exe") if selected_Browser.lower() == "chrome"
+            else BrowserManager.get_browser_path("firefox") if selected_Browser.lower() == "firefox"
+            else BrowserManager.get_browser_path("msedge.exe") if selected_Browser.lower() == "edge"
+            else BrowserManager.get_browser_path("dragon.exe")  
+        )
+    
+
+        if selected_Browser.lower() == "firefox":
+            ensure_web_ext_installed()
+
+        DevLogger.info("browser path   :",   browser_path    or "Non trouvé")
+
+        # return browser_path;
+        EXTRACTION_THREAD = ExtractionThread(
+            data_list, SESSION_ID, entered_number, browser_path , window ,selected_Browser , Isp , unique_id , output_json_final
+        )
+        # data_list, SESSION_ID, entered_number, Browser_path, main_window ,selected_Browser,Isp , unique_id , output_json_final
+        EXTRACTION_THREAD.progress.connect(lambda msg: print(msg))
+        EXTRACTION_THREAD.finished.connect(lambda: QMessageBox.information(window, "Terminé", "L'extraction est terminée."))
         EXTRACTION_THREAD.finished.connect(lambda: self.Extraction_Finished(window))
+        EXTRACTION_THREAD.stopped.connect(lambda msg: QMessageBox.warning(window, "Arrêté", msg))
+        EXTRACTION_THREAD.start()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
